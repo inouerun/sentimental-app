c49f556ede33b5a65423e95d1cca66153723c2a9
inouerun <itnyr1203@gmail.com>
2020-12-12 21:07:05 +0900

    npm intall jest, angular/cdk & ng g component output-area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputAreaComponent } from './components/input-area/input-area.component';
+import { OutputAreaComponent } from './components/output-area/output-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputAreaComponent
+    InputAreaComponent,
+    OutputAreaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/output-area/output-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-output-area',
+  templateUrl: './output-area.component.html',
+  styleUrls: ['./output-area.component.sass']
+})
+export class OutputAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
