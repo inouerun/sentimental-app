dbce3ff879ccbcd318af77ad548e7680a84e478e
inouerun <itnyr1203@gmail.com>
2020-12-12 20:37:23 +0900

    add component, service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputAreaComponent } from './components/input-area/input-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputAreaComponent
   ],
   imports: [
     BrowserModule,
